56a2d2fbb915504ff8ae221aab0e415f38644f7c
Yuki <140987699+yukiito0914@users.noreply.github.com>
2024-10-30 19:47:24 -0400

    Create sorting_scaffolds.sh

---
+++ structural_annotation/sorting_scaffolds.sh
@@ -0,0 +1,3 @@
+# Sorting scaffolds by their size
+conda activate seqkit
+sort -lr2 /home/yuki_ito/work2/Tasks/RepeatMask/Jamaican/draft_genome.sort.rename.softmasked.fasta > /home/yuki_ito/work2/Tasks/RepeatMask/Jamaican/draft_genome.sort.rename.softmasked_new.fasta
