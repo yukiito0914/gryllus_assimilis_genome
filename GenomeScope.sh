52f8ed53418320f8754fe427ccace6fcc2146514
Yuki <140987699+yukiito0914@users.noreply.github.com>
2024-10-30 18:04:24 -0400

    Create GenomeScope.sh

---
+++ GenomeScope.sh
@@ -0,0 +1,18 @@
+conda activate genomescope
+
+### Settong ###
+jellyfish_path="/home/yuki_ito/work2/App/jellyfish-2.3.0"
+genomescope_path="/home/yuki_ito/work2/App/genomescope"
+fastq_path="/home/yuki_ito/work2/Src/dorado_Gryllus_assimillis/dorado_J_filtered.fastq"
+WORK_DIR="/home/yuki_ito/work2/Tasks/genomescope/Jamaican"
+thread=
+
+### genomescope ###
+cd $jellyfish_path
+
+### fastq
+jellyfish count -C -m 21 -s 1000000000 -t $thread $fastq_path -o $WORK_DIR/reads.jf
+
+jellyfish histo -t $thread $WORK_DIR/reads.jf > $WORK_DIR/reads.histogram
+cd $genomescope_path
+Rscript genomescope.R $WORK_DIR/reads.histogram 21 301 $WORK_DIR/output_dir
