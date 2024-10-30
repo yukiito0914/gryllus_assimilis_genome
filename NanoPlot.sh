0d1460af34982d3ac8087cf5439ea6c3078d4c82
Yuki <140987699+yukiito0914@users.noreply.github.com>
2024-10-30 18:10:18 -0400

    Create NanoPlot.sh

---
+++ NanoPlot.sh
@@ -0,0 +1,8 @@
+###setting###
+INPUT_FASTQ="/home/yuki_ito/work2/Src/dorado_Gryllus_assimillis/dorado_J_filtered.fastq"
+OUT_DIR="/home/yuki_ito/work2/Tasks/Gryllus_assimilis/nanoplot/output_dorado_filtered"
+THREADS="32"
+
+conda activate nanoplot
+###NanoPlot###
+NanoPlot --fastq $INPUT_FASTQ -t $THREADS -o $OUT_DIR
