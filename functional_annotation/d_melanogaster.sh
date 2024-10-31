3a7fb674b188efd1f894e899a53ffa59a5cf3f17
Yuki <140987699+yukiito0914@users.noreply.github.com>
2024-10-30 20:11:58 -0400

    Create d_melanogaster.sh

---
+++ functional_annotation/d_melanogaster.sh
@@ -0,0 +1,25 @@
+### setting ###
+WORK_DIR=/home/yuki_ito/work2/Tasks/blast/Jamaican
+query=/home/yuki_ito/work2/Data/Jamaican/gass_v1.0.protein.fasta
+db_dir=/work2/kataoka/fanflow_db
+db_name=(
+Drosophila_melanogaster.BDGP6.32.pep.all
+)
+evalue_blast=1e-10
+threads=
+
+### blastp ###
+conda activate blast
+cd $WORK_DIR
+for i in ${db_name[@]}; do
+
+blastp \
+ -query $query \
+ -db $db_dir/${i}.fa \
+ -outfmt 6 \
+ -max_target_seqs 1 \
+ -evalue $evalue_blast \
+ -num_threads $threads \
+ -out $WORK_DIR/blastp_out_${i}.txt
+
+done
